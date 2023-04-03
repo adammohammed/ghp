54e05769166d6d2b40396d42dbedadb23cb977bb
Adam Mohammed <admohammed@equinix.com>
2023-04-03 14:46:56 -0400

    testing out emacs packaging

---
+++ something.el
@@ -0,0 +1,23 @@
+;;; something.el --- Something and something else.
+
+;; Author: Adam Mohammed <adammoham@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((magit "3.3.0"))
+;; Keywords: hypermedia
+;; URL: https://github.com/adammohammed/ghp
+
+;;; Commentary:
+
+;; This package provides some simple helpers to open github pages.
+
+
+;;; Code:
+
+
+(defun adam-something-loaded ()
+  "This function does something."
+  (message "doing something"))
+
+
+(provide 'something)
+;;; something.el ends here
